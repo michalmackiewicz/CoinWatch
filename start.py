805f4200e0631395f9bc5754b8d3c1dc820588c1
Michal Mackiewicz <m.mackiewicz@protonmail.com>
2017-12-06 21:27:47 +0100

    common date for all coins

--- start.py
+++ start.py
@@ -5,18 +5,19 @@ import json
 import sqlite3
 from datetime import date, datetime
 
-def add_to_base(crypto, value):
+def add_to_base(crypto, value, created=datetime.now()):
   db = sqlite3.connect('rates.db')
   # Get a cursor object
   cursor = db.cursor()
   # Check if table does not exist and create it
   cursor.execute("CREATE TABLE IF NOT EXISTS "+crypto+" (id INTEGER PRIMARY KEY, created timestamp, value REAL)")
 
-  cursor.execute("INSERT INTO "+crypto+"(created, value) VALUES(?,?)", (datetime.now(), value))
+  cursor.execute("INSERT INTO "+crypto+"(created, value) VALUES(?,?)", (created, value))
 
   db.commit()
 
   db.close()
+
 
 def get_value(crypto, fiat):
 
@@ -31,20 +32,13 @@ def get_value(crypto, fiat):
 def main():
 
   print("Average values from bitbay")
+  current_time = datetime.now()
   cryptos = ["BTC","ETH","LSK","LTC","GAME","DASH","BCC"]
 
   for crypto in cryptos:
     val = get_value(crypto,"PLN")
     print(crypto+" "+str(val))
-    add_to_base(crypto, val)
-  # print(get_value("ETH","PLN"))
-  # print(get_value("LSK","PLN"))
-  # print(get_value("LTC","PLN"))
-  # print(get_value("GAME","PLN"))
-  # print(get_value("DASH","PLN"))
-  # print(get_value("BCC","PLN"))
-  
+    add_to_base(crypto, val, current_time)
 
 if __name__ == '__main__':
   main()
-
